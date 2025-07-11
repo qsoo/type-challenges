1664b4cf993e04d3cbc2b2e1e4451f279aadc33b
qsoo <qsoo3810@gmail.com>
2025-07-11 17:01:47 +0900

    Solve 00043, 00189

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,7 +22,36 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyAwaited<T> = any
+/**
+ * 1. Awaited<T>는 다음과 같이 타입 선언이 되어 있다.
+ * type MyAwaited<T> = T extends null | undefined ? T : // special case for `null | undefined` when not in `--strictNullChecks` mode
+ *   T extends object & { then(onfulfilled: infer F, ...args: infer _): any } ? // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+ *     F extends ((value: infer V, ...args: infer _) => any) ? // if the argument to `then` is callable, extracts the first argument
+ *       Awaited<V> : // recursively unwrap the value
+ *       never : // the argument to `then` was not callable
+ *     T // non-object or non-thenable
+ *
+ *  2. 따라서 재귀적으로 다음과 같이 정의할 수 있다.
+ * type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U> ? U extends PromiseLike<any> ? MyAwaited<U> : U : never
+ *
+ *  3. 하지만 Promise가 아닌 경우에 타입 에러가 발생하지 않음.
+ * MyAwaited<number> // 타입 에러 발생하지 않음
+ *
+ *  4. Promise만 허용하려면 다음과 같이 타입을 엄격하게 지정할 수 있다.
+ * type MyAwaited<T extends PromiseLike<any>> =
+ *   T extends PromiseLike<infer V>
+ *     ? V extends PromiseLike<any>
+ *       ? MyAwaited<V>
+ *       : V
+ *     : never
+ */
+
+type MyAwaited<T extends PromiseLike<any>> =
+  T extends PromiseLike<infer V>
+    ? V extends PromiseLike<any>
+      ? MyAwaited<V>
+      : V
+    : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
